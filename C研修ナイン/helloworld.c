e652ebaca39962a7e52d1c10cbc68724e1e6870f
Naing009 <sithuyan-n@gr.m-fielders.com>
2025-10-09 16:47:43 +0900

    adding C code test

---
+++ C研修ナイン/helloworld.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World。\n");
+    printf("first line。\n");
+    //printf("\n"); comments
+    /* 
+    this 
+    is 
+    also 
+    a comment 
+    */
+    printf("first line。\n");
+
+    int a = 1;
+    int b = 5;
+    int c = a + b;
+
+    printf("Value of C is %d.\n", c);
+    return 0;
+}
